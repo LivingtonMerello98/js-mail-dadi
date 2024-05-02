4cf21be7154a0760953c5994ee3de38a5144de2f
LivingtonMerello98 <livington.merello@gmail.com>
2024-05-02 17:40:15 +0200

    [upd] boolean operations

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -8,4 +8,11 @@ const numUser = Math.floor(Math.random() * 6) + 1;
 console.log(numUser);
 
 
-
+//condizionali
+if(numCpu > numUser){
+    console.log("CPU win");
+}else if (numCpu === numUser){
+    console.log("odd");
+}else{
+    console.log( "user win");
+}
